784d06489a2baaeba79dabcf680ee1c880204fa2
anrd30 <aniruddhshreyas@gmail.com>
2025-10-09 15:11:31 +0530

    feat: wrap app with AuthProvider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/tailwind.css";
 import App from "./MainApp.tsx";
+import { AuthProvider } from "./auth/AuthContext.tsx";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
